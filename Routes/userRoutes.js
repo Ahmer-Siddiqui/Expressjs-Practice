28308e4bbae8a2b30e4dac69e84e0822601447b9
Ahmer-Siddiqui <siddiquiahmer970@gmail.com>
2023-06-06 13:22:37 +0500

    new  modification

--- Routes/userRoutes.js
+++ Routes/userRoutes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const {userData,registerUser} = require("./userController")
+const {userData,registerUser} = require("../Controller/userController")
 const router = express.Router();
 
 router.route("/").get(userData)
--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const PORT = 4000;
 const bodyParser = require('body-parser');
-const router = require('./userRoutes')
+// const router = require('./Routes/userRoutes')
 const app = express();
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.json());
 
-app.use("/v1/api", router)
+// app.use("/v1/api", router)
+app.use("/v1/api", require('./Routes/userRoutes'))
 
 app.listen(PORT, ()=>{
     console.log(`Server is Working on port: ${PORT}`);
